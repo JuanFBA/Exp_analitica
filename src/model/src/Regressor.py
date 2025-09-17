4a9f9cfd5ecbf679d86631b94de645463ab0e69c
Juan Felipe Bolivar Agudelo <jfbolivara@eafit.edu.co>
2025-09-17 04:52:59 +0000

    ryta

--- src/model/src/Regressor.py
+++ src/model/src/Regressor.py
@@ -2,12 +2,14 @@
 from torch import nn
 
 class Regressor(nn.Module):
-    def __init__(self, in_dim: int, hidden: list[int] = [128, 64]):
+    def __init__(self, in_dim: int, hidden: list[int] = [128, 64], dropout: float = 0.0):
         super().__init__()
         layers = []
         last = in_dim
         for h in hidden:
             layers += [nn.Linear(last, h), nn.ReLU()]
+            if dropout > 0:
+                layers += [nn.Dropout(p=dropout)]
             last = h
         layers += [nn.Linear(last, 1)]  # salida escalar para regresión
         self.net = nn.Sequential(*layers)
